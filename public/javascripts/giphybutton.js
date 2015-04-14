29dc114b6bb9011f267241f063cbb76ea6ac582f
pmeinusch <patrick.meinusch@seitenbau.com>
2015-04-14 13:12:41 +0200

    Use giphy api using https

--- public/javascripts/giphybutton.js
+++ public/javascripts/giphybutton.js
@@ -132,7 +132,7 @@ var GiphyButton = (function () {
     var search = getSearchQuery(searchString);
 
     if(search) {
-      jqxhr = $.get( "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=dc6zaTOxFJmzC")
+      jqxhr = $.get( "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=dc6zaTOxFJmzC")
       .done(function(result) {
         appendGifUrls("#giphyUrls", result.data)
       })
